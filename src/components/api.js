d2e0c07a42f107c42cc58dc6564d4f919162724d
Vaishnavi187 <vaishnavisingh2005.college@gmail.com>
2025-02-01 18:08:21 +0530

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -1,6 +1,6 @@
 export const fetchQuizData = async () => {
     try {
-      const response = await fetch("https://thingproxy.freeboard.io/fetch/https://api.jsonserve.com/Uw5CrX  "); 
+      const response = await fetch("https://corsproxy.io/?https://api.jsonserve.com/Uw5CrX";); 
       const data = await response.json();
       return data;
     
